41103b84f518629b6293cdf48f08ac7dbbe2af5d
Ismail El Ghazi <Ismail47el@gmail.com>
2021-12-02 18:39:04 +0100

    TP4

---
+++ src/main/java/Rappel/rapple1.java
@@ -0,0 +1,20 @@
+package Rappel;
+public class rapple1 {
+    static int vitesse = 10;
+    rapple1(int v){
+        this();
+        vitesse+=v;
+    }
+    rapple1(){
+        vitesse++;
+    }
+    public static void main(String[] args) {
+        rapple1 r1=new rapple1(11);
+        //r1 =  11+10+1 --> r1=22
+        rapple1 r2=new rapple1(10);
+        //r2 =10+1+r1 --> r2=33
+        System.out.println(r1.vitesse);
+        System.out.println(r2.vitesse);
+    }
+
+}
--- src/main/java/ma/education/tp3/TestSpringIocApplicationContextcontenant.java
+++ src/main/java/ma/education/tp3/TestSpringIocApplicationContextcontenant.java
@@ -1,12 +1,28 @@
 package ma.education.tp3;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ma.education.tp3.Client;
 public class TestSpringIocApplicationContextcontenant {
     public static void main(String[] args) {
-        ApplicationContext appContext=new AbstractApplicationContext() {
-        };
+    ApplicationContext appContext=new AbstractApplicationContext() {
+        @Override
+        protected void refreshBeanFactory() throws BeansException, IllegalStateException {
+
+        }
+
+        @Override
+        protected void closeBeanFactory() {
+
+        }
+
+        @Override
+        public ConfigurableListableBeanFactory getBeanFactory() throws IllegalStateException {
+            return null;
+        }
+    };
         new ClassPathXmlApplicationContext("spring.xml");
         Client client = (Client ) appContext.getBean("clt");
     }
